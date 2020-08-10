f00a2ca1125c5695491f927ebb93ebfabc47c50a
Simen A. W. Olsen <cobraz@cobraz.no>
2020-08-10 14:17:57 +0200

    fix: reference issue

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { UpdateVersion } from './updateVersion';
 // Inputs
 const args = {
   service: core.getInput('service', { required: true }),
-  servicesFilePath: core.getInput('services_path', { required: true }),
-  newVersion: core.getInput('new_version') as string | './services.json',
+  servicesFilePath: core.getInput('services_path') || './services.json',
+  newVersion: core.getInput('new_version', { required: true }),
 };
 
 new UpdateVersion(args).updateAndSave();
